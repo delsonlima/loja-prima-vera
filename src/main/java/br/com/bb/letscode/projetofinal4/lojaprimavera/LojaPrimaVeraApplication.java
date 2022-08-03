8e1f318e514020b03d31a386582c9226f13bd58d
F2370438 Delson Douglas Barbosa Lima <delson.lima@bb.com.br>
2022-08-03 15:44:48 -0300

    Estrutura inicial do projeto

---
+++ src/main/java/br/com/bb/letscode/projetofinal4/lojaprimavera/LojaPrimaVeraApplication.java
@@ -0,0 +1,13 @@
+package br.com.bb.letscode.projetofinal4.lojaprimavera;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LojaPrimaVeraApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LojaPrimaVeraApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/com/bb/letscode/projetofinal4/lojaprimavera/LojaPrimaVeraApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.bb.letscode.projetofinal4.lojaprimavera;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LojaPrimaVeraApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
